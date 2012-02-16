906b109e773774b6f8d9ffd9ac3adc45bc03b2cd
David Gageot <david@gageot.net>
2012-02-16 16:36:32 +0100

    Puzzle006

---
+++ src/main/java/net/gageot/puzzler/Puzzle006.java
@@ -0,0 +1,14 @@
+package net.gageot.puzzler;
+
+/**
+ * Multicast.<br/>
+ * http://www.dartlang.org/articles/puzzlers/chapter-1.html#6
+ */
+public final class Puzzle006 {
+	private Puzzle006() {
+	}
+
+	public static void print() {
+		Utils.print((int) (char) (byte) -1);
+	}
+}
