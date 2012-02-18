f39a58346b50ba1c405153c0e669c854b266e3c3
David Gageot <david@gageot.net>
2012-02-18 09:47:40 +0100

    Puzzle022

---
+++ src/main/java/net/gageot/puzzler/Puzzle022.java
@@ -0,0 +1,16 @@
+package net.gageot.puzzler;
+
+/**
+ * Dupe of URL.<br/>
+ * http://my.safaribooksonline.com/book/programming/java/032133678x/puzzlers-with-character/ch03lev1sec12
+ */
+public final class Puzzle022 {
+	private Puzzle022() {
+	}
+
+	public static void main(String[] args) {
+		System.out.print("iexplore:");
+		http://www.google.com;
+		System.out.println(":maximize");
+	}
+}
